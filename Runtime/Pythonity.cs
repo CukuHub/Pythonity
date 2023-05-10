bd95cf085881cc15c6362bfe84f95e956d63830f
Besjan <xhikab@gmail.com>
2023-05-11 00:26:40 +0200

    Refactor

--- Runtime/Pythonity.cs
+++ Runtime/Pythonity.cs
@@ -22,19 +22,22 @@ namespace Cuku.Pythonity
                         Runtime.PythonDLL = Path.Combine(home, new DirectoryInfo(home).Name + ".dll");
                         PythonEngine.Initialize();
                     }
-                    // Add site-packages path where libraries are installed
-                    else if (line.StartsWith("command ="))
-                    {
-                        var site_packages = Path.Combine(line.Substring(line.IndexOf("-m venv") + "-m venv".Length).Trim(), @"Lib\site-packages");
-                        using (Py.GIL())
-                        {
-                            dynamic py_sys = Py.Import("sys");
-                            py_sys.path.insert(0, site_packages);
-                        }
-                    }
+                }
+
+                // Add site-packages path where libraries are installed
+                var site_packages = Path.Combine(Path.GetDirectoryName(pyvenv), @"Lib\site-packages");
+                using (Py.GIL())
+                {
+                    dynamic py_sys = Py.Import("sys");
+                    py_sys.path.insert(0, site_packages);
                 }
             }
         }
+
+        /// <summary>
+        /// The Python code of a Package is located in "StreamingAssets/Py[PackageName]".
+        /// </summary>
+        public static string PythonPackage(this string package) => $"Py{package}";
 
         /// <summary>
         /// Execute python code from specified script with optional input parameters and receive optional output data.
@@ -43,14 +46,14 @@ namespace Cuku.Pythonity
         /// <param name="directory">Optional directory inside StreamingAssets where to search for the script file.</param>
         /// <param name="output">Optional output object returned by the Python code.
         /// <para>IMPORTANT: Is the developers responsability to cast the object to the correct type!</para></param>
-        /// <param name="input">Optional input parameters passed to the python code before executing it.</param>
+        /// <param name="input">Optional input parameters passed to the python code.</param>
         /// <returns></returns>
-        public static async UniTask<object> Execute(string script, string directory = "", string output = null, params KeyValuePair<string, object>[] input)
+        public static async UniTask<object> Execute(this string script, string directory = "", string output = null, params KeyValuePair<string, object>[] input)
         {
             // Load script code from StreamingAssets
             var code = await System.IO.File.ReadAllTextAsync($"{script}.py"
                 .GetStreamingAssets(directory)[0]).AsUniTask();
-
+                
             using (Py.GIL())
             {
                 using (var scope = Py.CreateScope())
